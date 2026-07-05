9761f73ef0be3e53bac2108184958e78ac41f08f
320387unimore <320387@studenti.unimore.it>
2026-07-05 21:50:11 +0200

    Delete PREVISIONI_RHU/call_function_from_here_TimeSeries_rhu.py

--- PREVISIONI_RHU/call_function_from_here_TimeSeries_rhu.py
+++
@@ -1,7 +0,0 @@
-from requests import post
-import json
-
-data = {'rh-1': 97, 'rh-2': 99, 'rh-3': 100, 'weekend01': 0}
-r = post('https://europe-west8-pcloud2026-495807.cloudfunctions.net/predict_rhu', data={'data': json.dumps(data)})
-print(r.status_code)
-print(r.text)
