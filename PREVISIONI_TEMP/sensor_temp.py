2186921f222c5875b9e87c23f121d8addc23914d
320387unimore <320387@studenti.unimore.it>
2026-07-05 21:47:56 +0200

    Clean up sensor_temp.py by removing unused imports
    Removed unused imports and adjusted comments for clarity.

--- PREVISIONI_TEMP/sensor_temp.py
+++ PREVISIONI_TEMP/sensor_temp.py
@@ -4,21 +4,15 @@ import time
 from pathlib import Path
 from requests import post
 
-import os
-import certifi
-
-# Forza Python a usare il bundle di certificati corretto ed esistente
-os.environ['REQUESTS_CA_BUNDLE'] = certifi.where()
-os.environ['CURL_CA_BUNDLE'] = certifi.where()
-
+#URL della Cloud Function che elabora le temperature e gestisce il retraining del modello
 F_URL = 'https://europe-west8-pcloud2026-495807.cloudfunctions.net/save_data_temp'
 CSV_PATH = Path(__file__).with_name('08_temp_out.csv')
 INTERVAL_SECONDS = 3
 
-
+#file CSV aperto in modalità di lettura 
 with CSV_PATH.open(newline='', encoding='utf-8') as csv_file:
-    reader = csv.DictReader(csv_file, delimiter=';')
-    for row in reader:
+    reader = csv.DictReader(csv_file, delimiter=';') #lettore speciale che trasforma le righe del CSV in dizionari, con le intestazioni come chiavi
+    for row in reader: #crea un dizionario con le righe del CSV, con le intestazioni come chiavi e i valori delle celle come valori; converte il dizionario in stringa JSON e invia tutto tramite una richiesta HTTP POST alla Cloud Function
         print(row)
         response = post(
             F_URL,
